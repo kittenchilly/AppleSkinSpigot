5d1fdab10045433475d33dbd3948df8922c1e2de
kittenchilly <mario.pikmin.zelda@gmail.com>
2021-12-05 14:17:30 -0600

    Update BukkitCraftPlayerFactory.java

--- src/main/java/com/jmatt/appleskinspigot/craftplayer/BukkitCraftPlayerFactory.java
+++ src/main/java/com/jmatt/appleskinspigot/craftplayer/BukkitCraftPlayerFactory.java
@@ -6,8 +6,8 @@ public class BukkitCraftPlayerFactory {
 
     public BukkitCraftPlayer getBukkitCraftPlayer(Server server) {
         String version = server.getBukkitVersion();
-        if (version.contains("1.17.1")) {
-            return new BukkitCraftPlayer_1_17_1();
+        if (version.contains("1.18")) {
+            return new BukkitCraftPlayer_1_18();
         } else {
             server.getLogger().warning(String.format("Unsupported server version: [%s]", version));
             return null;
