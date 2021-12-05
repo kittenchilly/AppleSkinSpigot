a7551a8d6a355152f58be7f75cf6849dc7e6bf05
kittenchilly <mario.pikmin.zelda@gmail.com>
2021-12-05 14:18:18 -0600

    Update BukkitCraftPlayer_1_18.java

--- src/main/java/com/jmatt/appleskinspigot/craftplayer/BukkitCraftPlayer_1_18.java
+++ src/main/java/com/jmatt/appleskinspigot/craftplayer/BukkitCraftPlayer_1_18.java
@@ -4,7 +4,7 @@ import net.minecraft.network.protocol.game.PacketPlayOutCustomPayload;
 import org.bukkit.craftbukkit.v1_18_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
-public class BukkitCraftPlayer_1_18_1 implements BukkitCraftPlayer {
+public class BukkitCraftPlayer_1_18 implements BukkitCraftPlayer {
 
     @Override
     public void sendPacket(Player player, PacketPlayOutCustomPayload packet) {
